877bd1d41d09b17f40c0e5585db009e5c0eadc09
amiramouelhi <mouelhiam23@gmail.com>
2025-08-19 11:45:22 +0100

    wip

--- apps/main/src/pages/workSpace.tsx
+++ apps/main/src/pages/workSpace.tsx
@@ -1,9 +1,11 @@
  
 import {Routes, Route } from 'react-router-dom'
 import Main from '../layouts/workSpace/MainPage';
+import Sidebar from '../layouts/workSpace/Sidebar';
 const WorkSpace = () => {
   return (
     <div>
+      <Sidebar/>
       <Routes>
         <Route path="main" element={<Main/>} />
       </Routes>
