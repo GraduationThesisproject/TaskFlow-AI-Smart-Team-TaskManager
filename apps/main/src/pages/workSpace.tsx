da411888f7a44cb949cfe2390860c8433dffec95
amiramouelhi <mouelhiam23@gmail.com>
2025-08-19 14:51:56 +0100

    wip

--- apps/main/src/pages/workSpace.tsx
+++ apps/main/src/pages/workSpace.tsx
@@ -1,16 +1,14 @@
- 
-import {Routes, Route } from 'react-router-dom'
+ import {Routes, Route } from 'react-router-dom'
 import Main from '../layouts/workSpace/MainPage';
-import Sidebar from '../layouts/workSpace/Sidebar';
+
 const WorkSpace = () => {
   return (
     <div>
-      <Sidebar/>
+      
       <Routes>
         <Route path="main" element={<Main/>} />
       </Routes>
-      
-     </div>
+    </div>
    );
  };
 
