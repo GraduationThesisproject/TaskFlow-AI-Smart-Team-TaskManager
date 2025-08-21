c203a5248cfc1ce5a30e93fb0e91ed965f358458
Jesser sekri <jessersekrii11@gmail.com>
2025-08-21 18:28:30 +0100

    done

---
+++ apps/admin/src/pages/SettingsPage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import SettingsLayout from '../layouts/SettingsLayout';
+
+const SettingsPage: React.FC = () => {
+  return <SettingsLayout />;
+};
+
+export default SettingsPage;
