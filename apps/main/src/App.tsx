02aea91fef3ab968cb1e54eb26176890e70de28b
amiramouelhi <mouelhiam23@gmail.com>
2025-08-21 10:46:40 +0100

    wip

--- apps/main/src/App.tsx
+++ apps/main/src/App.tsx
@@ -3,13 +3,10 @@ import { ThemeProvider } from '@taskflow/theme';
 import { useTheme } from './hooks/useTheme';
 
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-<<<<<<< HEAD
 // import { SpacePage } from './pages/space.page';
 import WorkSpace from './pages/workSpace';
-=======
 import { SpacePage } from './pages/space.page';
 import Dashboard from './pages/Dashboard';  
->>>>>>> 5ec308c8c72e7ff020513a784e06bdc26f8b3375
 
 function AppContent() {
   const { theme, toggleTheme } = useTheme();
@@ -40,13 +37,10 @@ function AppContent() {
       <main className="bg-gradient-to-br from-background via-muted/50 to-background">
 
         <Routes>
-<<<<<<< HEAD
           {/* <Route path="/space/*" element={<SpacePage />} /> */}
           <Route path="/workspace/*" element={<WorkSpace />} />
-=======
           <Route path="/space/*" element={<SpacePage />} />
           <Route path="/dashboard/*" element={<Dashboard/>} />
->>>>>>> 5ec308c8c72e7ff020513a784e06bdc26f8b3375
         </Routes>
       </main>
     </div>
