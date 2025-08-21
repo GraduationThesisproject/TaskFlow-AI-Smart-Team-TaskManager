f6b50fb85c2b9f2e728512e311d28a3851e70055
Hazem Douzi <hz.douzi20@gmail.com>
2025-08-21 15:02:48 +0100

    before merge bassem

--- apps/main/src/main.tsx
+++ apps/main/src/main.tsx
@@ -2,16 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { store } from './store'
-import { ThemeProvider } from '@taskflow/theme'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <ThemeProvider defaultTheme="dark">
+
         <App />
-      </ThemeProvider>
+
     </Provider>
   </StrictMode>,
 )
