50f8c53b65944aaf0d6cacf74df7cb3814eb32cb
Hazem Douzi <hz.douzi20@gmail.com>
2025-08-22 14:55:29 +0100

    bmc

--- apps/main/src/pages/LandingPage.tsx
+++ apps/main/src/pages/LandingPage.tsx
@@ -1,12 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 import  SignIn  from "../layouts/Landing/SignIn";
 import SignUp from "../layouts/Landing/SignUP";
-import  LandingNavbar  from "../components/common/navbar/landingnav/navbar";
 
 export const LandingPage = () => {
     return (
         <div>
-            <LandingNavbar />
             <Routes>
                 <Route path="/" element={<div>Landing Page</div>} />
                 <Route path="/signin" element={<SignIn />} />
