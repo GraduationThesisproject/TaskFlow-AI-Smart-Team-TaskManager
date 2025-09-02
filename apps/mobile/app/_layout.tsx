f36fba90ff8110b52c2d9172bf76ef0d92ae8f01
charfed <charfedchaibi@gmail.com>
2025-09-02 10:47:44 +0100

    erros to work on

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,5 +1,18 @@
 import { Stack } from "expo-router";
+import { Platform } from "react-native";
+import { ThemeProvider } from "../Theme/ThemeProvider";
 
-export default function RootLayout() {
-  return <Stack />;
+function MaybeThemeProvider({ children }: { children: React.ReactNode }) {
+  if (Platform.OS === "web") {
+    return <ThemeProvider defaultTheme="dark">{children}</ThemeProvider>;
+  }
+  return <>{children}</>;
+}
+
+export default function Layout() {
+  return (
+    <MaybeThemeProvider>
+      <Stack />
+    </MaybeThemeProvider>
+  );
 }
--- apps/mobile/app/index.tsx
+++ apps/mobile/app/index.tsx
@@ -1,5 +1,5 @@
 import { Text, View } from "react-native";
-
+import  {Button} from  "../ui/Button"
 export default function Index() {
   return (
     <View
@@ -9,7 +9,9 @@ export default function Index() {
         alignItems: "center",
       }}
     >
+      <Text>TaskFlow</Text>
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Button variant="default">Button</Button>
     </View>
   );
 }
