c2955b6e3908250bd9d1be1dc4e443cc22fc72eb
amiramouelhi <mouelhiam23@gmail.com>
2025-09-08 11:03:24 +0100

    jesser merge

--- apps/mobile/app/login.tsx
+++ apps/mobile/app/login.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ScrollView } from 'react-native';
-import { Text, View, Card } from '@/components/Themed';
+import { Text, View, Card, Button, ButtonText } from '@/components/Themed';
 import { useThemeColors } from '@/components/ThemeProvider';
 import { TextStyles } from '@/constants/Fonts';
 import LoginForm from '@/components/auth/LoginForm';
