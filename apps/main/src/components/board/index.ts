4965ef0d4893fef6def2fa845abecc6935dbedf5
Hazem Douzi <hz.douzi20@gmail.com>
2025-09-11 11:40:17 +0100

    132123

--- apps/main/src/components/board/index.ts
+++ apps/main/src/components/board/index.ts
@@ -1,3 +1,4 @@
 export { SubNavigation } from './SubNavigation';
 export { TaskDetailModal } from './TaskDetailModal';
 export { CommentItem } from './CommentItem';
+export { AddChecklistModal } from './AddChecklistModal';
